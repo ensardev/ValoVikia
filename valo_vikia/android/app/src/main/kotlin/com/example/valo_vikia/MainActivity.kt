fb608d058739907ba6ddd1e62fa33220d514a12c
ensardev <ensarakkuzey@gmail.com>
2022-01-11 23:27:37 +0300

    Create flutter project and add data file for agents.

---
+++ valo_vikia/android/app/src/main/kotlin/com/example/valo_vikia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.valo_vikia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ valo_vikia/lib/data/agent_strings.dart
@@ -0,0 +1,159 @@
+class Strings {
+  Strings() {
+    print('Olusturuldu');
+  }
+  static const String APP_NAME = "ValoVikia";
+
+  static const List<String> AGENTS_NAME = [
+    "Brimstone",
+    "Viper",
+    "Omen",
+    "Killjoy",
+    "Cypher",
+    "Sova",
+    "Sage",
+    "Phoenix",
+    "Jett",
+    "Reyna",
+    "Raze",
+    "Breach",
+    "Skye",
+    "Yoru",
+    "Astra",
+    "KAY/O",
+    "Chamber",
+    "Neon"
+  ];
+
+  static const List<String> AGENTS_TYPE = [
+    "Kontrol Uzmanı",
+    "Kontrol Uzmanı",
+    "Kontrol Uzmanı",
+    "Gözcü",
+    "Gözcü",
+    "Öncü",
+    "Gözcü",
+    "Düellocu",
+    "Düellocu",
+    "Düellocu",
+    "Düellocu",
+    "Öncü",
+    "Öncü",
+    "Düellocu",
+    "Kontrol Uzmanı",
+    "Öncü",
+    "Gözcü",
+    "Düellocu"
+  ];
+
+  static const List<String> AGENTS_SPEC = [
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Brimstone, düşmanlara zarar vermek ve ekip arkadaşlarını güçlendirmek için yanıcı el bombaları, sis perdesi hava saldırıları ve hızlı ateş uyandıran kuvvet işaretlerini kullanan güçlü bir komutandır. Etkileyici Uydu Saldırısı ultisi, çemberin dışına yeterince hızlı çıkmayanları yok eden devasa bir lazer çağırıyor.\n\n"
+        "\tÜlke: A.B.D\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Kontrol Uzmanı\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Zehir ve kimyasal savaşla uğraşan bir avcı. Savaş alanına asit saçan patlayan mermilerin yanı sıra bir zehirli gaz yayıcı ve geçişi önlemek için zehirli bir gaz perdesi var. Nihai yeteneği, konumunu gizleyen ve içinde dolaşan düşmanları vurgulayabilen zehirli bir bulut yayar.\n\n"
+        "\tÜlke: A.B.D\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Kontrol Uzmanı\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Gizemli kökenleri olan Omen, düşmanlarının vizyonunu yakın görüşlülükle görüş alanlarına vuran bir küre ve yakındaki herkesin vizyonunu gizlemek için patlayan başka şeylerle engellemeye odaklanıyor. Ayrıca kısa bir mesafeye ışınlayabilir veya haritanın herhangi bir yerine bir gölge olarak ışınlanmak için ultisini kullanabilir.\n\n"
+        "\tÜlke: Belirsiz\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Kontrol Uzmanı\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Alman dâhi Killjoy, kendi icatlarından oluşan cephaneliğiyle savaş meydanını kontrol altına alır. Cihazlarının verdiği hasar rakiplerini durdurmaya yetmezse robotlarının yardımıyla rakiplerini zayıflatarak onların ortadan kaldırılmalarını kolaylaştırır.\n\n"
+        "\tÜlke: Almanya\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Gözcü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: İstihbarat senin işinse, Cypher senin Ajanın olabilir. Bu Faslı gözetleme Ajanı, içinde sıkışan düşmanları tuzağa düşüren ve yerlerini ortaya çıkaran yeteneklere ve yapılanları uzaktan izlemek için haritaya yerleştirebileceği gerçek bir casus kamerasına sahiptir. Nihai yeteneği, rakip takımdaki kalan oyuncuların yerini geçici olarak ortaya çıkarmak için yeni bir düşman cesedinden bilgi almasıdır.\n\n"
+        "\tÜlke: Fas\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Gözcü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Bir okçuluk uzmanı olarak, yayını düşman konumlarını ortaya çıkaran veya yakındaki rakipleri şok eden farklı cıvatalarla donatabilir. Savaş alanını araştırmak için keşif dronu da kullanabilirken, ultisi duvarların içinden de geçebilen üç ölümcül ok atışı yapmasına izin verir.\n\n"
+        "\tÜlke: Rusya\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Öncü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Bugüne kadarki en önemli destek karakteri Çin'den geliyor ve müttefikleri iyileştiren, düşmanları yavaşlatan ve bir maçın akışını engellemek için yerde engeller oluşturan küreler getiriyor. Nihai yeteneği, düşmüş bir müttefiki diriltmesine izin verir, bu da yeniden doğma olmayan bir oyunda önemlidir.\n\n"
+        "\tÜlke: Çin\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Gözcü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Ateşli İngiliz, savaş alanını esasen uygun gördüğü şekilde yeniden şekillendirmek için alevin gücünü kullanabilir. Düşmanlara hasar veren ve onu iyileştiren patlayan ateş topları, görüşü engelleyen bir ateş duvarı ve kör edici bir ateş patlamasıyla Phoenix, maçın akışını yönlendirmede harikadır. Nihai yeteneği, süresi boyunca öldürülürse ona yeniden doğma yeteneği verir, aksi takdirde yapmayacağı cesur hareketler yapması için onu harekete geçirir.\n\n"
+        "\tÜlke: Birleşik Krallık\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Düellocu\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Kore'den, harekete her şeyden çok öncelik veren çevik bir savaşçı olan Jett geliyor. Yetenekleri arasında ışınlanma tabanlı bir rüzgar gibi yeteneği ve daha yüksek çıkıntılara ulaşmasını sağlayan hafifle hareketi var. Ayrıca görüş alanlarını engelleyen bir duman bombası yeteneğine ve ağır hasar veren ve hareket ederken bile isabetli kalan güçlü bir Keskin Fırtına ultisine sahiptir.\n\n"
+        "\tÜlke: Güney Kore\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Düellocu\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Reyna, Ruh Hasadı yeteneğine sahiptir. Reyna tarafından öldürülen düşmanlar 3 saniye süren Ruh Kürelerini geride bırakır. Ruh Hasadı yetenekleri, ortak kullanım havuzuna sahip Sömür ve Azlet'ten oluşur. Ayrıca, kullandığı anda oyuncuları bir süreliğine kör eden Kem Göz yeteneğine sahiptir.\n\n"
+        "\tÜlke: Meksika\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Düellocu\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Raze'nin oyun tarzı, VALORANT'daki diğer ajanların çoğuna kıyasla çok agresiftir, çünkü yeteneklerinin çoğu, istihbarat toplama veya destek yerine hareketlilik ve hasara odaklanıyor. Yetenekleri başlıca hasar vermeye yönelik olan birkaç ajandan birisidir ve söz konusu yetenekler için topluluk tarafından sıklıkla eleştirilir.\n\n"
+        "\tÜlke: Brezilya\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Düellocu\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Breach'in ana hüneri, tüm yeteneklerini duvarlardan, çatılardan veya arazilerden haritanın geometrisi aracılığıyla kullanabilmesidir. Bu yetenek seti deneyimli oyuncuları haritaları iyi bildikleri için ödüllendirir. Yeteneklerinin çoğu kitle kontrolüne dayalı olmasına rağmen, yetenekleri agresif bir oyun tarzını ödüllendiriyor gibi görünüyor.\n\n"
+        "\tÜlke: İsveç\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Öncü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Aramıza Avustralya'dan katılan Skye, beraberinde getirdiği vahşi hayvan ordusuyla birlikte çatışmayı iyiden iyiye kızıştırıyor. Düşmana ayak bağı olan hayvanları ve takım arkadaşlarını iyileştirme yetisiyle, takımının gücünü pekiştiriyor ve takımdaki herkesi kolluyor.\n\n"
+        "\tÜlke: Avustralya\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Öncü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Japon Yoru, gerçeklikte açtığı geçitlerden geçerek düşman hattına sinsice sızıyor. Yanılsamalardan ve kaba kuvvetten eşit oranda faydalanarak hedeflerini göz açıp kapayıncaya kadar indiriyor.\n\n"
+        "\tÜlke: Japonya\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Düellocu\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Ganalı ajan Astra, kozmosun enerjisini kullanarak savaş meydanını lehine değiştirebiliyor. Astral yolculuğu üzerinde tam kontrole sahip olması ve engin stratejik öngörüsüyle, düşmanın bir sonraki hamlesinden her zaman birkaç ışık yılı önde.\n\n"
+        "\tÜlke: Gana\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Kontrol Uzmanı\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Alternatif bir gelecekten gelen KAY/O, savaş alanına düşmanlarını bastırma yeteneğine sahip yeni bir değişken getiriyor, radyantları geçici olarak güçsüz hale getiriyor veya düşman teknolojisini bir an için devre dışı bırakıyor. Bu sayesinde, KAY/O birincil direktifini yerine getirebilecek kapasitededir: düşmanı etkisiz hale getirmek ve Dünya'sını korumak.\n\n"
+        "\tÜlke: Alternatif Zaman Çizgisinde Dünya\n\n"
+        "\tCinsiyet: Erkek (Robot)\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Öncü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Her daim şık ve tam teçhizat dolaşan Fransız silah tasarımcısı Chamber, ölümcül isabetliliğiyle rakipleri ortadan kaldırıyor. Özel yeteneklerini kullanarak görüş mesafesi sağlar, uzaktaki rakipleri avlar ve her plana mükemmel bir şekilde uyum sağlar.\n\n"
+        "\tÜlke: Fransa\n\n"
+        "\tCinsiyet: Erkek\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Gözcü\n\n",
+    "\tGenel Özellikler\n\n\n"
+        "\tÖzet: Filipinli Ajan Neon, şoke edici bir hızla öne atılır ve bedeninin ürettiği biyoelektrik dalgalarını düşmanların üstüne salar. Düşmanları gafil avlamak için yarışırcasına koşar ve hepsini yıldırım hızıyla indirir.\n\n"
+        "\tÜlke: Filipinler\n\n"
+        "\tCinsiyet: Kadın\n\n"
+        "\tBağlantı: Valorant Protokolü\n\n"
+        "\tRol: Düellocu\n\n"
+  ];
+}
